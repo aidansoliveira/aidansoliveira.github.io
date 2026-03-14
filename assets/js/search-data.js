6c6219b558b6f9e8688ddda2a8a0d4924b736b0d
aidansoliveira <136005409+aidansoliveira@users.noreply.github.com>
2026-03-14 16:14:28 +0000

    Deploying to gh-pages from @ aidansoliveira/aidansoliveira.github.io@31f1423de28bb91badc0e7ed056c1c356c322656 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-research",
           title: "Research",
-          description: "My goal is to do research in the following areas: neighborhood effects, urban network models, spatial equilibrium models, agglomeration economies, spatial trade dynamics, innovation across space, local public finance, zoning, urban economic history, and related spatial and urban topics. Below are projects that I am actively working on. These began as thesis topics, and I am continuing to develop them with the goal of getting them to a state where they can be published. Please feel free to reach out with any questions.",
+          description: "Research page",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
