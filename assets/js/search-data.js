064546f1b48460657117c0cd3265ae9715ad1ecc
aidansoliveira <136005409+aidansoliveira@users.noreply.github.com>
2026-07-29 18:26:55 +0000

    Deploying to gh-pages from @ aidansoliveira/aidansoliveira.github.io@46a275d20a07968afa3a4f7cca4ebde9eb783b6b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-aidan-soares-oliveira",
+    title: "Aidan Soares Oliveira",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
